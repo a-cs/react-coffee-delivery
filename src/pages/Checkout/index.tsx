efd10611956900c97c84d2f8bbc879e0f716575e
Anderson Carneiro Sousa <acarneirosousa@gmail.com>
2023-07-18 09:22:26 -0300

    creating project

---
+++ src/pages/Checkout/index.tsx
@@ -0,0 +1,11 @@
+import { CheckoutContainer } from './styles'
+
+export function Checkout() {
+    return (
+        <CheckoutContainer>
+            <h1>Checkout</h1>
+        </CheckoutContainer>
+    )
+}
+
+export default Checkout
